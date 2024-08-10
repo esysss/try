c582aefd663dd398f44808885abe6f10ff93f7db
esysss <esysss@yahoo.com>
2024-08-10 13:31:22 -0300

    The buttons became into a three way switch

--- app.js
+++ app.js
@@ -53,56 +53,6 @@ document.getElementById('ct-head-no').addEventListener('click', function() {
         document.getElementById('divHemorrhage').style.display = 'none';
 });
 
-// document.getElementById('ct-head-yes').addEventListener('click', function() {
-//     document.getElementById('divInfraction').style.display = 'block';
-//     document.getElementById('divHemorrhage').style.display = 'block';
-// });
-
-// document.getElementById('finish-button').addEventListener('click', function () {
-
-//     demographicsData = getInfo("patient-info-form");
-//     symptomsData = getInfo("symptoms-exam-form");
-//     investigationData = getInfo("investigation-imaging-form");
-    
-//     const summaryContent = `
-//       <h3>Demographics and Past Medical History</h3>
-//       <p>Age: ${demographicsData.age}</p>
-//       <p>Gender: ${demographicsData.gender}</p>
-//       <p>Myocardial Infarction: ${demographicsData.Myocardial}</p>
-//       <p>Arrhythmia: ${demographicsData.Arrhythmia}</p>
-//       <p>Stroke or TIA: ${demographicsData.Stroke}</p>
-//       <p>HTN: ${demographicsData.HTN}</p>
-//       <p>DM: ${demographicsData.DM}</p>
-//       <p>Smoking: ${demographicsData.Smoking}</p>
-//       <p>Functional Status: ${demographicsData.Functional}</p>
-//       <p>Code Status: ${demographicsData.Status}</p>
-
-//       <h3>Symptoms and Exam</h3>
-//       <p>Date/Time: ${symptomsData['date-time']}</p>
-//       <p>Weakness: ${symptomsData.Weakness}</p>
-//       <p>Side of Weakness: ${symptomsData.Side}</p>
-//       <p>Aphasia: ${symptomsData.Aphasia}</p>
-//       <p>Facial Drool: ${symptomsData.Facial}</p>
-//       <p>Visual Symptoms: ${symptomsData.Visual}</p>
-//       <p>Days of Symptoms: ${symptomsData.Days}</p>
-//       <p>Resolution of Symptoms: ${symptomsData.Resolution}</p>
-
-//       <h3>Investigation and Imaging</h3>
-//       <p>CTA-Carotid: ${investigationData.CTA}</p>
-//       <p>CT-Head: ${investigationData.CT}</p>
-//       <p>Infraction: ${investigationData.Infraction}</p>
-//       <p>Hemorrhage: ${investigationData.Hemorrhage}</p>
-//       <p>ECG Atrial Fibrillation: ${investigationData.ECG}</p>
-//     `;
-
-//     document.getElementById('summary-content').innerHTML = summaryContent;
-
-//     document.getElementById('investigation-page').style.display = 'none';
-//     document.getElementById('summary-page').style.display = 'block';
-
-//     // Additional processing and triage logic here
-// });
-
 function getInfo(path) {
     const form = document.getElementById(path);
     const formData = new FormData(form);
